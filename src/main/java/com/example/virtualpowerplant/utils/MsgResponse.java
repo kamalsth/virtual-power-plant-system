24b3189cf05b015896fb10030e9f4c1f6a63ce26
kamalsth <kamalshrestha5618@gmail.com>
2024-07-28 12:55:09 +0545

    response added

---
+++ src/main/java/com/example/virtualpowerplant/utils/MsgResponse.java
@@ -0,0 +1,10 @@
+package com.example.virtualpowerplant.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MsgResponse {
+    public String message;
+}
